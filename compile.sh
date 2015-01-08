d7214d006c161efec1de92c3d1b75fa1c635dc17
Lim Ding Wen <limdingwen@icloud.com>
2015-01-08 19:03:53 +0800

    Added rm jar, so that it actually updates.

--- compile.sh
+++ compile.sh
@@ -2,4 +2,5 @@
 
 javac -verbose -d . -classpath ".:lib/guava-13.0.1.jar:lib/jinput.jar:lib/jna.jar:lib/lwjgl.jar:lib/lwjgl_util.jar:lib/PNGDecoder.jar" `find src | grep .java`
 
+rm SpaceCubes.jar
 jar cvfm SpaceCubes.jar MANIFEST `find com | grep .class`
